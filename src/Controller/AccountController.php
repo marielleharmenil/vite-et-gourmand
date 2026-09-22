9510a2bdd60670ce4de4be889f7c0793287df2e3
Marielle Harmenil <167091734+marielleharmenil@users.noreply.github.com>
2026-09-22 08:00:46 +0200

    feat: add customer account and order history

---
+++ src/Controller/AccountController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AccountController extends AbstractController
+{
+    #[Route('/account', name: 'app_account')]
+    public function index(): Response
+    {
+        $this->denyAccessUnlessGranted('ROLE_USER');
+        $user = $this->getUser();
+        $orders = $user->getCustomerOrders();
+        return $this->render('account/index.html.twig', [
+            'user' => $user,
+            'orders' => $orders,
+        ]);
+    }
+}
